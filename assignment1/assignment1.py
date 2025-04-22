75b4e6815545c21b71f759f2b9bc05848a853108
jf <jpf0628@gmail.com>
2025-04-22 15:06:51 -0700

    WIP: assignment 1

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,1 +1,14 @@
-# Write your code here.
+# Task 1: Hello
+def hello():
+  return "Hello!"
+
+# Task 2: Greet with a Formatted String
+def greet(name):
+  return f"Hello, {name}!"
+
+
+# Task 3: Calculator
+
+
+# pytest -v -x assignment1-test.py 
+# can use just -x, adding -v lists the passing tests
